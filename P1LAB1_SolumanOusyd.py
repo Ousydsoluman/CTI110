891cb19b49a3a17538b1d174b1dddf4ec03bc11d
Ousydsoluman <solumano6798@student.faytechcc.edu>
2025-09-19 16:07:14 -0400

    Add files via upload

---
+++ P1LAB1_SolumanOusyd.py
@@ -0,0 +1,9 @@
+print("Hello world")
+#OusydSoluman
+#09/19/2025
+#P1LAB1 - Python Input and Output
+#This Program asks the user for their first  and last name and displays a  wlecome message
+
+first_name=input("Enter your first name:")
+last_name=input("Enter your Last name:")
+print("Hello,", first_name, last_name+ "!welcome the CTI-110")
